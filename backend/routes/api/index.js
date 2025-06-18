9e873a656ddc7bd83a3ceb2df9683c1236a6197b
Mcode4 <lamllc20@gmail.com>
2025-06-18 12:30:36 -0400

    api routes update

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,5 +1,9 @@
 const router = require("express").Router();
 
 router.use("/users", require("./users"))
-router.use("posts", require("./posts"))
+router.use("/posts", require("./posts"))
+router.use("/followers", require("./followers"))
+
+
+module.exports = router
 
