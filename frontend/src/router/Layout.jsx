a00dcdc10e1ea8cae51feb2c9b06d6ded791fe4c
Mcode4 <lamllc20@gmail.com>
2025-07-14 21:15:52 -0400

    redux added

--- frontend/src/router/Layout.jsx
+++ frontend/src/router/Layout.jsx
@@ -1,3 +1,21 @@
 import { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom"
 import { useDispatch } from "react-redux"
+import { ModalProvider, Modal } from "../context/Modal";
+
+export default function Layout() {
+    // const dispatch = useDispatch();
+    // const [isLoaded, setIsLoaded] = useState(false);
+    // useEffect(() => {
+    //     dispatch('To be finished')
+    // })
+
+    return (
+        <>
+            <ModalProvider>
+                <Outlet />
+                <Modal />
+            </ModalProvider>
+        </>
+    )
+}
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,19 @@
+import Layout from "./Layout";
+import LandingPage from "../components/LandingPage";
+import HomePage from "../components/HomePage";
 
-// export const router = createBrowserRouter([
-//     {
-//         e
-//     }
-// ])
+export const router = createBrowserRouter([
+    {
+        element: <Layout />,
+        children: [
+            {
+                path: '/',
+                element: <LandingPage />
+            },
+            {
+                path: '/home',
+                element: <HomePage />
+            },
+        ]
+    }
+]);
